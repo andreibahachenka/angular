c0e05ec3108d0b67f97abda5ea8b993ba783bc04
Andrey Bogachenko <bogachq@mail.ru>
2018-04-01 22:23:47 +0300

    logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
+import { LocalStorageConfig } from '../../../app-config/locastorage.config';
+import { RoutesConfig } from '../../../app-config/routes.config';
+import { Router } from '@angular/router';
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -12,6 +16,12 @@ export class HeaderComponent {
     // public appToolBarLogo: string = PathConfig.mainLogoPath;
 
     constructor(
+        private router: Router
     ){
+    }
+
+    public logout() {
+        localStorage.removeItem(LocalStorageConfig.token);
+        this.router.navigate([RoutesConfig.login]);
     }
 }
