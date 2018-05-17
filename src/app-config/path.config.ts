920253846ef2ba1d149813de89d9b28027d11ea5
iskorzun <iskorzun@gmail.com>
2018-05-17 17:29:32 +0300

    change server url

--- src/app-config/path.config.ts
+++ src/app-config/path.config.ts
@@ -1,41 +1,41 @@
 export const PathConfig = {
-    mainPath: 'http://46.30.42.15:8066/v1/admin',
+    mainPath: 'http://jticonnect.pr3.eu:8066/v1/admin',
 
     //users
-    authEndpoint: 'http://46.30.42.15:8066/v1/admin/auth',
-    getUsersEndpoint: 'http://46.30.42.15:8066/v1/admin/users',
-    createUserEndpoint: 'http://46.30.42.15:8066/v1/admin/users/create',
-    updateUserEndpoint: 'http://46.30.42.15:8066/v1/admin/users/update',
-    removeUserEndpoint: 'http://46.30.42.15:8066/v1/admin/users/remove',
+    authEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/auth',
+    getUsersEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/users',
+    createUserEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/users/create',
+    updateUserEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/users/update',
+    removeUserEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/users/remove',
 
     //Administrators
-    getAdministratorsEndpoint: 'http://46.30.42.15:8066/v1/admin/',
-    createAdministratorEndpoint: 'http://46.30.42.15:8066/v1/admin/create',
-    updateAdministratorEndpoint: 'http://46.30.42.15:8066/v1/admin/update',
-    removeAdministratorEndpoint: 'http://46.30.42.15:8066/v1/admin/remove',
+    getAdministratorsEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/',
+    createAdministratorEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/create',
+    updateAdministratorEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/update',
+    removeAdministratorEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/remove',
 
     //Lotteries
-    getLotteriesEndpoint: 'http://46.30.42.15:8066/v1/admin/lotteries',
-    createLotteryEndpoint: 'http://46.30.42.15:8066/v1/admin/lotteries/create',
-    updateLotteryEndpoint: 'http://46.30.42.15:8066/v1/admin/lotteries/update',
-    removeLotteryEndpoint: 'http://46.30.42.15:8066/v1/admin/lotteries/remove',
+    getLotteriesEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/lotteries',
+    createLotteryEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/lotteries/create',
+    updateLotteryEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/lotteries/update',
+    removeLotteryEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/lotteries/remove',
 
     //Image converter
-    uploadImageEndpoint:'http://46.30.42.15:8066/v1/services/photo/upload',
+    uploadImageEndpoint:'http://jticonnect.pr3.eu:8066/v1/services/photo/upload',
 
     //Get cities
-    getCityEndpoint: 'http://46.30.42.15:8066/v1/services/cities',
+    getCityEndpoint: 'http://jticonnect.pr3.eu:8066/v1/services/cities',
 
     //Quizzes
-    getQuizzesEndpoint: 'http://46.30.42.15:8066/v1/admin/quizzes',
-    createQuizEndpoint: 'http://46.30.42.15:8066/v1/admin/quizzes/create',
-    updateQuizEndpoint: 'http://46.30.42.15:8066/v1/admin/quizzes/update',
+    getQuizzesEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/quizzes',
+    createQuizEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/quizzes/create',
+    updateQuizEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/quizzes/update',
 
     //Special quizzes
-    getSpecialQuizzesEndpoint: 'http://46.30.42.15:8066/v1/admin/quizzes/special',
-    createSpecialQuizEndpoint: 'http://46.30.42.15:8066/v1/admin/quizzes/special/create',
-    updateSpecialQuizEndpoint: 'http://46.30.42.15:8066/v1/admin/quizzes/special/update',
+    getSpecialQuizzesEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/quizzes/special',
+    createSpecialQuizEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/quizzes/special/create',
+    updateSpecialQuizEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/quizzes/special/update',
 
     //Games
-    getGamesEndpoint: 'http://46.30.42.15:8066/v1/admin/games',
+    getGamesEndpoint: 'http://jticonnect.pr3.eu:8066/v1/admin/games',
 };
